1cde247210d72f49bb2acdb1f155951b433335b3
Stargirl3 <annachrysa_rutter@hotmail.com>
2021-12-31 08:22:30 +0200

    1.10

--- Part-1/unicafe/src/App.js
+++ Part-1/unicafe/src/App.js
@@ -19,7 +19,7 @@ const NoStatistics = ({ text, counter }) => {
 }
 
 //component that renders the feedback results, if feedback has been given
-const Statistics = ({ feedback, text, counter }) => {
+const StatisticLine = ({ feedback, text, counter }) => {
   if (counter !== 0) return <p>{text} {feedback}</p>
   else return null
 }
@@ -55,12 +55,12 @@ const App = () => {
       <Button handleClick={() => setBad(bad + 1)} text='bad' />
       <Display title={heading2} />
       <NoStatistics text='No feedback given' counter={totalFeedback()} />
-      <Statistics feedback={good} text='good' counter={totalFeedback()} />
-      <Statistics feedback={neutral} text='neutral' counter={totalFeedback()} />
-      <Statistics feedback={bad} text='bad' counter={totalFeedback()} />
-      <Statistics feedback={totalFeedback()} text='all' counter={totalFeedback()} />
-      <Statistics feedback={averageScore()} text='average' counter={totalFeedback()} />
-      <Statistics feedback={percentOfGoodFeedback() + '%'} text='positive' counter={totalFeedback()} />
+      <StatisticLine feedback={good} text='good' counter={totalFeedback()} />
+      <StatisticLine feedback={neutral} text='neutral' counter={totalFeedback()} />
+      <StatisticLine feedback={bad} text='bad' counter={totalFeedback()} />
+      <StatisticLine feedback={totalFeedback()} text='all' counter={totalFeedback()} />
+      <StatisticLine feedback={averageScore()} text='average' counter={totalFeedback()} />
+      <StatisticLine feedback={percentOfGoodFeedback() + '%'} text='positive' counter={totalFeedback()} />
     </div>
   )
 }
