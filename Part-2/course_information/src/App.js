76124cdb4490c1fad5e9df2ba4b87ce5ef6586d9
Stargirl3 <annachrysa_rutter@hotmail.com>
2022-01-06 13:52:27 +0200

    2.4

--- Part-2/course_information/src/App.js
+++ Part-2/course_information/src/App.js
@@ -1,7 +1,7 @@
 import React from 'react'
 
 //renders header
-const Header = ({ course }) => <h1>{course.name}</h1>
+const Header = ({ course }) => <h2>{course.name}</h2>
 
 //uses map() to render array items via the Part component
 const Content = ({ course }) => (
@@ -36,34 +36,62 @@ const Course = ({ course }) => (
 
 //main app
 const App = () => {
-  const course = {
-    id: 1,
-    name: 'Half Stack application development',
-    parts: [
-      {
-        name: 'Fundamentals of React',
-        exercises: 10,
-        id: 1
-      },
-      {
-        name: 'Using props to pass data',
-        exercises: 7,
-        id: 2
-      },
-      {
-        name: 'State of a component',
-        exercises: 14,
-        id: 3
-      },
-      {
-        name: 'Redux',
-        exercises: 11,
-        id: 4
-      }
-    ]
-  }
+  const courses = [
+    {
+      name: 'Half Stack application development',
+      id: 1,
+      parts: [
+        {
+          name: 'Fundamentals of React',
+          exercises: 10,
+          id: 1
+        },
+        {
+          name: 'Using props to pass data',
+          exercises: 7,
+          id: 2
+        },
+        {
+          name: 'State of a component',
+          exercises: 14,
+          id: 3
+        },
+        {
+          name: 'Redux',
+          exercises: 11,
+          id: 4
+        }
+      ]
+    },
+    {
+      name: 'Node.js',
+      id: 2,
+      parts: [
+        {
+          name: 'Routing',
+          exercises: 3,
+          id: 1
+        },
+        {
+          name: 'Middlewares',
+          exercises: 7,
+          id: 2
+        }
+      ]
+    }
+  ]
 
-  return <Course course={course} />
+
+  return (
+    <>
+      <h1>Web Development Curriculum</h1>
+      <>
+      {courses.map(course => 
+        <Course key={course.id} course={course} />
+      )}
+      </>
+    </>
+  )
 }
 
 export default App
