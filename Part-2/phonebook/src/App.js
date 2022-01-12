1baa01986e3f0755ed9736671fb9d37765647347
Stargirl3 <annachrysa_rutter@hotmail.com>
2022-01-12 06:40:07 +0200

    2.8

--- Part-2/phonebook/src/App.js
+++ Part-2/phonebook/src/App.js
@@ -1,44 +1,56 @@
 import React, { useState } from 'react'
 
-const Person = ({ person }) => {
-  return (
-    <div>{person.name}</div>
-  )
-}
+
+const Person = ({ person }) => <div>{person.name} - {person.number}</div>
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas' }
-  ]) 
+    { name: 'Arto Hellas', number: '39-44-5323523', id: 1 }
+  ])
   const [newName, setNewName] = useState('')
+  const [newNumber, setNewNumber] = useState('')
 
-  /* checks if the new name already exists in the phonebook and if it does it issues a warning and doesn't add it. Otherwise, it adds it to the phonebook*/
+  /* checks if the new name and number already exist in the phonebook and if they do it issues a warning and doesn't add them. Otherwise, they get added to the phonebook, alog with a new ID*/
   const addPerson = (event) => {
     event.preventDefault()
-    console.log(persons);
-    
+
     const nameExists = (person) => person.name === newName
-    if (persons.some(nameExists)) alert(`${newName} is already added to the phonebook`)
+    const numberExists = (person) => person.number === newNumber
+
+    if (persons.some(nameExists)) alert(`${newName} is already in the phonebook`)
+    else if (persons.some(numberExists)) alert(`The number: ${newNumber} is already in the phonebook`)
     else {
       const personObject = {
-        name: newName
+        name: newName,
+        number: newNumber,
+        id: persons.length + 1
       }
       setPersons(persons.concat(personObject))
     }
-    setNewName('')    
+    setNewName('')
+    setNewNumber('')
   }
 
   const handleNameChange = (event) => setNewName(event.target.value)
+  const handleNumberChange = (event) => setNewNumber(event.target.value)
+
 
   return (
     <>
       <h2>Phonebook</h2>
       <form onSubmit={addPerson}>
         <div>
-          name: 
-          <input 
-          value={newName}
-          onChange={handleNameChange}
+          name:
+          <input
+            value={newName}
+            onChange={handleNameChange}
+          />
+        </div>
+        <div>
+          number:
+          <input
+            value={newNumber}
+            onChange={handleNumberChange}
           />
         </div>
         <div>
@@ -48,7 +60,7 @@ const App = () => {
       <h2>Numbers</h2>
       <div>
         {persons.map(person =>
-          <Person key={person.name} person={person} />
+          <Person key={person.id} person={person} />
         )}
       </div>
     </>
