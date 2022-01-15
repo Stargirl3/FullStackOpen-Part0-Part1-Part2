0a26efcb8456daa2eacc3f30337e6da084755b62
Stargirl3 <annachrysa_rutter@hotmail.com>
2022-01-15 07:44:14 +0200

    2.10

--- Part-2/phonebook/src/App.js
+++ Part-2/phonebook/src/App.js
@@ -1,10 +1,56 @@
 import React, { useState } from 'react'
 
 
-const Person = (props) => <div>{props.person.name}, {props.person.number}</div>
+/*Renders a search box on the page and handles the event which filters the list of contacts to match the string being searched*/
+const Filter = ({ handleSearchChange }) => (
+  <div>
+    Find a contact by name:
+    <input
+      onChange={handleSearchChange}
+    />
+  </div>
+)
 
 
+//Renders the contacts in the phonebok
+const Persons = ({ persons }) => (
+  <div>
+    {persons.map(person =>
+      <div key={person.id}>{person.name}, {person.number}</div>
+    )}
+  </div>
+)
+
+
+//Renders form for adding a new contact to phonebook and handles new additions to the list
+const PersonForm = (props) => (
+  <form onSubmit={props.handleSubmit}>
+    <div>
+      name:
+      <input
+        value={props.nameValue}
+        onChange={props.handleNameChange}
+      />
+    </div>
+    <div>
+      number:
+      <input
+        value={props.numberValue}
+        onChange={props.handleNumberChange}
+      />
+    </div>
+    <div>
+      <button type="submit">add</button>
+    </div>
+  </form>
+)
+
+
+
+//Main App
 const App = () => {
+
+  /* pieces of State for setting the updated list of contacts, adding a new name and number, searching for a name, and showing the entire phonebook or not*/
   const [persons, setPersons] = useState([
     { name: 'Arto Hellas', number: '39-44-5323523', id: 1 },
     { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
@@ -15,6 +61,7 @@ const App = () => {
   const [newNumber, setNewNumber] = useState('')
   const [stringToFind, setStringToFind] = useState('')
   const [showAll, setShowAll] = useState(true)
+
 
 
 
@@ -41,11 +88,13 @@ const App = () => {
 
 
 
+  //sets new name 
   const handleNameChange = (event) => setNewName(event.target.value)
 
+  //sets new number
   const handleNumberChange = (event) => setNewNumber(event.target.value)
 
-  /* sets the string being typed as 'stringToFind' after it converts it to lowercase. If the search box is empty it sets 'showAll' as TRUE. Otherwise, if someone is currently searching for a name, it sets it as FALSE*/
+  /* sets the string being typed, as 'stringToFind', after it converts it to lowercase. If the search box is empty it sets 'showAll' as TRUE. Otherwise, if someone is currently searching for a name, it sets it as FALSE*/
   const handleSearchChange = (event) => {
     setStringToFind(event.target.value.toLowerCase())
 
@@ -53,51 +102,45 @@ const App = () => {
     else setShowAll(false)
   }
 
+
+
+
   /*If showAll === TRUE it shows the entire phonebook, otherwise it shows a new array that only contains contacts whose name contains the string being currently searched */
   const personsToShow = showAll
     ? persons
     : persons.filter(person => {
       if (person.name.toLowerCase().indexOf(stringToFind) !== -1) return true
+      else return false
     })
+
+
+
 
 
   return (
     <>
-      <h1>Phonebook</h1>
-      <h2>Search</h2>
-      <div>
-        Find a contact by name:
-        <input
-          onChange={handleSearchChange}
-        />
-      </div>
-      <h2>Add a new entry</h2>
-      <form onSubmit={addPerson}>
-        <div>
-          name:
-          <input
-            onChange={handleNameChange}
-          />
-        </div>
-        <div>
-          number:
-          <input
-            value={newNumber}
-            onChange={handleNumberChange}
-          />
-        </div>
-        <div>
-          <button type="submit">add</button>
-        </div>
-      </form>
-      <h2>Numbers</h2>
-      <div>
-        {personsToShow.map(person =>
-          <Person key={person.id} person={person} />
-        )}
-      </div>
+      <h2>Phonebook</h2>
+
+      <Filter handleSearchChange={handleSearchChange} />
+
+      <h3>Add a new entry</h3>
+
+      <PersonForm
+        handleSubmit={addPerson}
+        nameValue={newName}
+        handleNameChange={handleNameChange}
+        numberValue={newNumber}
+        handleNumberChange={handleNumberChange}
+      />
+
+      <h3>Numbers</h3>
+
+      <Persons persons={personsToShow} />
     </>
   )
 }
+
+
+
 
 export default App
